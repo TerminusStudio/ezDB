bf6d0f7dad52e355b82c2ccde6fb8cda1a98fbd3
Sugavanas <sugavanas@terminus.studio>
2021-04-24 15:12:22 +0800

    load.php file

---
+++ load.php
@@ -0,0 +1,34 @@
+<?php
+
+$ezDBPath = __DIR__ . '/src';
+
+//Load Interfaces
+require_once $ezDBPath . '/Interfaces/DriverInterface.php';
+
+//Load Exceptions
+require_once $ezDBPath . '/Exceptions/Exception.php';
+require_once $ezDBPath . '/Exceptions/ConnectionException.php';
+require_once $ezDBPath . '/Exceptions/DriverException.php';
+require_once $ezDBPath . '/Exceptions/ModelMethodException.php';
+require_once $ezDBPath . '/Exceptions/QueryException.php';
+
+//Load Drivers
+require_once $ezDBPath . '/Drivers/MySQLiDriver.php';
+require_once $ezDBPath . '/Drivers/PDODriver.php';
+
+//Load Connection
+require_once $ezDBPath . '/DatabaseConfig.php';
+require_once $ezDBPath . '/Connection.php';
+require_once $ezDBPath . '/Connections.php';
+require_once $ezDBPath . '/DB.php';
+
+//Load Builder
+require_once $ezDBPath . '/Query/Processor/Processor.php';
+require_once $ezDBPath . '/Query/Builder/Builder.php';
+require_once $ezDBPath . '/Query/Builder/JoinBuilder.php';
+require_once $ezDBPath . '/Query/Builder/RelationshipBuilder.php';
+require_once $ezDBPath . '/Query/Raw.php';
+
+//Load Model
+require_once $ezDBPath . '/Models/Relationship.php';
+require_once $ezDBPath . '/Models/Model.php';
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+//Use load.php file instead of composer autoload to make sure the load.php file stays updated.
+require_once __DIR__ . '/../load.php';
 
 $dbConfig = require_once('env.php');
 $dummyData = file_get_contents(__DIR__ . '/Data/dummy.sql');
